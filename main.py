10d283b654b6466f0db882ec74f1f92b26af07aa
Samuel Carlsson <samuel.carlsson@gmail.com>
2020-07-13 11:40:51 +0200

    spring model

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import itertools
+import sys
 
 from github import Github
 import cairo
@@ -11,11 +12,36 @@ TAU = 2 * np.pi
 class Simulation:
     def __init__(self):
         self.t = 0
-        self.positions = np.empty((0, 2))
+        #self.positions = np.empty((0, 2))
+        n = 10
+        self.positions = np.random.rand(n, 2) * 100
         self.springs = []
 
     def velocities(self, positions, t: float):
-        return 0
+        # compute spring forces
+
+        v = np.zeros(positions.shape)
+        # find indices annd lengths
+        ii = [i for i, _, _ in self.springs]
+        jj = [j for _, j, _ in self.springs]
+        ll = [l for _, _, l in self.springs]
+        stiffness = 1
+        mass = 1
+    
+        # compute deltas
+        di = positions[jj] - positions[ii]
+        dj = positions[ii] - positions[jj]
+
+        # compute lengths
+        norms = np.linalg.norm(di, axis=-1)
+
+        # compute force magnitudes
+        forces = (norms - ll) * stiffness
+
+        # compute accelerations and accumulate
+        v[ii] += (di / norms[:, None]) * forces[:, None] / mass
+        v[jj] += (dj / norms[:, None]) * forces[:, None] / mass
+        return v
 
     def step(self, dt: float):
         p = self.positions
@@ -39,7 +65,8 @@ def clear(target: cairo.ImageSurface, color=(1, 1, 1)) -> None:
 
 
 class Renderer:
-    def __init__(self, simulation, resolution, bg=(0, 0, 0)):
+    def __init__(self, output, simulation, resolution, bg=(0, 0, 0)):
+        self.output = output
         self.simulation = simulation
         width, height = resolution
         self.surface = cairo.ImageSurface(cairo.Format.RGB24, width, height)
@@ -49,17 +76,17 @@ class Renderer:
         surface = self.surface
         
         ctx = cairo.Context(surface)
-        ctx.set_source_rgb(0.42, 0.22, 4)
+        ctx.set_source_rgb(0.42, 0.22, 1)
         #w, h = surface.get_width(), surface.get_height()
 
         clear(surface, color=self.bg)
         
         for x, y in self.simulation.positions:
             ctx.move_to(x, y)
-            ctx.arc(x, y, 1, 0, TAU)
+            ctx.arc(x, y, 5, 0, TAU)
             ctx.fill()
 
-        f.write(surface.get_data())
+        self.output.write(surface.get_data())
 
 
 # or using an access token
@@ -75,7 +102,19 @@ reconstruction = g.get_repo("Volumental/Reconstruction")
 commits = reconstruction.get_commits()
 
 simulation = Simulation()
-renderer = Renderer(simulation, (320, 200))
+simulation.springs.append((3, 4, 20))
+simulation.springs.append((1, 2, 20))
+simulation.springs.append((5, 6, 20))
+renderer = Renderer(
+    sys.stdout.buffer,
+    simulation,
+    (320, 200),
+    bg=(1, 1, 1))
 
-for commit in itertools.islice(commits, 10):
-    #print(commit.files)
+for _ in range(200):
+    simulation.step(dt=0.1)
+    renderer.render()
+
+#for commit in itertools.islice(commits, 10):
+#    for f in commit.files:
+#        print(dir(f))
