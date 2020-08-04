118c69df1bb9ceffa5389f8e669dc5479fcdc90c
Samuel Carlsson <samuel.carlsson@volumental.com>
2020-08-04 08:35:26 +0200

    refactor: base types to own file

--- codestorm/fetch.py
+++ codestorm/fetch.py
@@ -1,11 +1,21 @@
 from github import Github
 
+from codestorm.commit import Commit, File, NamedUser
+
 class Fetcher:
     def commits(self, repo_slug):
         return []
 
 
 class GithubAPI:
+    @staticmethod
+    def to_commit(commit) -> Commit:
+        return Commit(
+            commit.sha,
+            commit.last_modified,
+            committer=NamedUser(commit.committer.login),
+            files=[File(f.filename, f.additions, f.changes, f.deletions) for f in commit.files])
+
     def commits(self, repo_slug):
         with open('.token') as f:
             token = f.read().strip()
--- codestorm/storage.py
+++ codestorm/storage.py
@@ -1,44 +1,9 @@
-from datetime import datetime
 import os
 from pathlib import Path
 import pickle
 from typing import Iterable
 
-
-class NamedUser:
-    def __init__(self, login):
-        self.login = login
-
-
-class File:
-    def __init__(self, filename, additions, changes, deletions):
-        self.filename = filename
-        self.additions = additions
-        self.changes = changes
-        self.deletions = deletions
-        
-
-class Commit:
-    def __init__(self, sha, last_modified, committer, files):
-        self.sha = sha
-        self.last_modified = last_modified
-        self.committer = committer
-        self.files = files
-
-    def __le__(self, other):
-        if other is None:
-            return False
-        if other.last_modified is None:
-            return False
-        if self.last_modified is None:
-            return True
-        return other.last_modified <= self.last_modified
-
-
-def last_modified(commit) -> datetime:
-    return datetime.strptime(
-        commit.last_modified,
-        '%a, %d %b %Y %H:%M:%S %Z')
+from codestorm.commit import NamedUser, File, Commit, last_modified
 
 
 # To store commits
