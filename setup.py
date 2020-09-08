a64a2c3039852c42d9ad578c37f32a6dc7935ce1
Samuel Carlsson <samuel.carlsson@volumental.com>
2020-09-08 22:08:49 +0200

    setup tools

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='codestorm',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'click',
+    ],
+    entry_points='''
+        [console_scripts]
+        codestorm=codestorm.main:cli
+    ''',
+)
